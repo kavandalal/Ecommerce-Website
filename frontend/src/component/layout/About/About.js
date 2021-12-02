6df44cfc5acb08fdc1e48f71222f66b408046e26
kavandalal <kavandalal003@gmail.com>
2021-12-02 12:41:42 +0530

    bug fix

--- frontend/src/component/layout/About/About.js
+++ frontend/src/component/layout/About/About.js
@@ -1,30 +1,3 @@
-// import React from "react";
-// import "./aboutSection.css";
-// import { Typography } from "@material-ui/core";
-
-// const About = () => {
-//   return (
-//     <div className="aboutSection">
-//       <div></div>
-//       <div className="aboutSectionGradient"></div>
-//       <div className="aboutSectionContainer">
-//         <Typography component="h1">About Us</Typography>
-
-//         <div>
-//           <div>
-//             <span>
-//             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-//             </span>
-//           </div>
-
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default About;
-
 import React from 'react';
 import './aboutSection.css';
 import { Button, Typography, Avatar } from '@material-ui/core';
